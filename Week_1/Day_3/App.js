b7461e8d9e6cb2b251b136570d44b34acb7f4556
Anay_Gupta <anayg.hestabit@gmail.com>
2025-12-03 17:31:15 +0530

    feat: added multiplication function

--- Week_1/Day_3/App.js
+++ Week_1/Day_3/App.js
@@ -9,5 +9,11 @@ function subtract(a, b) {
   return a - b;
 }
 
+function multiply(a, b) {
+  return a * b;
+}
+
 console.log("2 + 3 =", add(2, 3));
 console.log("5 - 2 =", subtract(5, 2));
+console.log("3 * 4 =", multiply(3, 4));
+console.log("All operations completed");
