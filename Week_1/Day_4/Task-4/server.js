7eec4b12c5e654c7c9f6afab32c5a24bd20360c7
Anay_Gupta <anayg.hestabit@gmail.com>
2025-12-05 19:31:13 +0530

    Task-3 and Task-4 Completed

---
+++ Week_1/Day_4/Task-4/server.js
@@ -0,0 +1,28 @@
+const http = require("http");
+const PORT = 3000;
+
+let counter = 0;
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "application/json");
+
+  if (req.url === "/ping") {
+    const timestamp = new Date().toISOString();
+    res.writeHead(200);
+    res.end(JSON.stringify({ timestamp }));
+  } else if (req.url === "/headers") {
+    res.writeHead(200);
+    res.end(JSON.stringify({ headers: req.headers }, null, 2));
+  } else if (req.url === "/count") {
+    counter++;
+    res.writeHead(200);
+    res.end(JSON.stringify({ count: counter }));
+  } else {
+    res.writeHead(404);
+    res.end(JSON.stringify({ error: "Not Found" }));
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running → http://localhost:${PORT}`);
+});
